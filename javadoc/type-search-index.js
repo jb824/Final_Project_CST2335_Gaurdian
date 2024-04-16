c8de689b4868cc187cd5d8c180fb8eb014e85e76
felicityspyder <felicityspyder@protonmail.com>
2024-04-15 22:00:21 -0400

    add javadoc after debugging aant pckg error caused by values-fr/strings.xml

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.final_project.data","l":"DBOpener"},{"p":"com.example.final_project.data","l":"Story"}];updateSearchResults();
