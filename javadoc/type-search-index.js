813513d6b0e77e70c03fce8180e795a51f6b7d67
felicityspyder <felicityspyder@protonmail.com>
2024-04-15 21:27:35 -0400

    update translation to fix bug

--- javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.final_project.data","l":"DBOpener"},{"p":"com.example.final_project.data","l":"Story"}];updateSearchResults();
